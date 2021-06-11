d3745c0d91cc6378b1cc609f4996a58b14f5377e
imad <abbadimad0123@gmail.com>
2021-06-11 11:43:35 +0100

    New Update

--- js/app.js
+++ js/app.js
@@ -1,28 +1,31 @@
-// The main button for home page 'my work' //
+// Variabl
 var myWork = document.getElementById("my-work");
-myWork.addEventListener("click", function(){
+var btnOpen = document.getElementById("open-btn");
+var btnClose = document.getElementById("close-btn");
+
+
+// The main button for home page 'my work'
+myWork.addEventListener("click", (event) =>{
     var elm = document.getElementById("skills");
     elm.scrollIntoView();
 })
 
 // Open the slid menu for phone //
-
-function openFunction(){
+btnOpen.addEventListener("click", (event) =>{
     var navPhone = document.getElementById("nav-phone");
     
     if (navPhone.style.display = "none"){
         navPhone.style.display = "flex";
     }
-}
+})
 
 // Close the slid menu for phone //
-
-function closeFunction(){
+btnClose.addEventListener("click", (event) =>{
     var navPhone = document.getElementById("nav-phone");
     
     if (navPhone.style.display = "flex"){
         navPhone.style.display = "none";
     }
-}
+})
 
-// End of script //
+// End Of Script //
