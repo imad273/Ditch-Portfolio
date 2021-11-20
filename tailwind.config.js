1c278754dd9db4784bcfdf7f445b7817b2430504
imad <abbadimad0123@gmail.com>
2021-11-20 10:37:30 +0100

    add new files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+   purge: [],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+      extend: {
+         colors: {
+            primary: "#3D34DB",
+            brightPrimary: "#EEEBFF",
+            heading: "#1F2937",
+            textPar: "#6B7280",
+            pink: "#FEF2F2"
+         }
+      },
+      fontFamily: {
+         Inter: ["Inter , sans-serif"]
+      },
+      container: {
+         center: true
+      }
+   },
+   variants: {
+      extend: {},
+   },
+   plugins: [],
+}
